1f4d4f770cbd7ee6d9bd9bc31c7de93de149b161
aOpenbrier <aopenbrier@gmail.com>
2018-11-07 23:14:14 -0800

    view all and view low inventory func

--- schmamazonManager.js
+++ schmamazonManager.js
@@ -11,19 +11,36 @@ dbsync.then(() => {
                 choices: ["View Products For Sale", "View Low Inventory", "Add To Inventory", "Add New Product"]
             })
                 .then(answers => {
-                    switch (answers.selection){
+                    switch (answers.selection) {
                         case "View Products For Sale":
-
-                        break
+                            console.log(`ITEM# | NAME                    | PRICE | STOCK `)
+                            results.forEach(product => {
+                                console.log(`------|-------------------------|-------|-------
+${product.dataValues.item_id}${Array(7 - product.dataValues.item_id.toString().length).join(" ")}| \
+${product.dataValues.product_name}${Array(25 - product.dataValues.product_name.length).join(" ")}| \
+${product.dataValues.price}${Array(7 - product.dataValues.price.toString().length).join(" ")}| \
+${product.dataValues.stock_quantity}`
+                                )
+                            }
+                            )
+                            break
                         case "View Low Inventory":
-
-                        break
+                            console.log(`ITEM# | NAME                     | STOCK `)
+                            results.forEach(product => {
+                                if (product.dataValues.stock_quantity < 5) {
+                                    console.log(`------|--------------------------|-------
+${product.dataValues.item_id}${Array(7 - product.dataValues.item_id.toString().length).join(" ")}| \
+${product.dataValues.product_name} ${Array(25 - product.dataValues.product_name.length).join(" ")}| \
+${product.dataValues.stock_quantity}`)
+                                }
+                            })
+                            break
                         case "Add To Inventory":
 
-                        break
+                            break
                         case "Add New Product":
-                        
-                        break
+
+                            break
                     }
                 })
         })
