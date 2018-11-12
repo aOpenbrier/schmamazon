fa8f6db4a831b245436e2c4d7a7670414a3fb2c2
aOpenbrier <aopenbrier@gmail.com>
2018-11-11 16:00:56 -0800

    add inventory fix

--- schmamazonManager.js
+++ schmamazonManager.js
@@ -9,7 +9,7 @@ dbsync.then(() => {
                 type: 'list',
                 name: 'selection',
                 message: 'Select a function',
-                choices: ["View Products For Sale", "View Low Inventory", "Add To Inventory", "Add New Product", "Exit"]
+                choices: ["View Products For Sale", "View Low Inventory", "Add Stock To Inventory", "Add New Product", "Exit"]
             })
                 .then(answers => {
                     switch (answers.selection) {
@@ -36,65 +36,70 @@ ${product.dataValues.stock_quantity}`)
                                 }
                             })
                             break
-                        case "Add To Inventory":
-                            inq.prompt({
-                                type: 'list',
-                                name: 'addInvName',
-                                message: 'Select product to restock',
-                                choices: productsArr.map(product => product.dataValues.product_name)
-                            })
-                                .then(answers => {
-                                    //find index of selected product, to get it's current stock
-                                    const index = productsArr.findIndex(product => product.product_name === answers.addInvName)
-                                    
-                                    inq.prompt({
-                                        name: 'addInvQuantity',
-                                        message: `Enter quantity of ${answers.addInvName} to add to inventory`
-                                    })
-                                        .then(answers => {
-                                            if (parseInt(answers.addInvQuantity)) {
-                                                const newStockQty = answers.addInvQuantity + productsArr[index].dataValues.stock_quantity
-                                                db.products.update({ stock_quantity: newStockQty }, { where: { product_name: answers.addInvSelection } })
-                                            } else {
-                                                console.log('Enter a Number.')
-                                                qtySelection(selectionName, currentStock)
-                                            }
-                                        })
-                                })
-                            break
-                        case "Add New Product":
-                                inq.prompt([
-                                    {
-                                        name: 'product_name',
-                                        message: 'Enter Item Name'
-                                    },
-                                    {
-                                        name: 'department_name',
-                                        message: "Enter department"
-                                    },
-                                    {
-                                        name: 'price',
-                                        message: "Enter price"
-                                    },
-                                    {
-                                        name: 'stock_quantity',
-                                        message: "Enter current stock quantity"
-                                    }
+                        case "Add Stock To Inventory":
+                            inq.prompt([
+                                {
+                                    type: 'list',
+                                    name: 'addInvName',
+                                    message: 'Select product to restock',
+                                    choices: productsArr.map(product => product.dataValues.product_name)
+                                },
+                                {
+                                    name: 'addInvQuantity',
+                                    message: `Enter quantity to add to inventory`
+                                }
                             ])
                             .then(answers => {
+                                //find index of selected product, to get it's current stock
+                                const index = productsArr.findIndex(product => product.product_name === answers.addInvName)
+                                if (!isNaN(answers.addInvQuantity)){
+                                    const newStockQty = parseInt(answers.addInvQuantity) + parseInt(productsArr[index].dataValues.stock_quantity)
+                                    db.products.update({ stock_quantity: newStockQty }, { where: { product_name: answers.addInvName } })
+                                    .then(r => console.log(`${answers.addInvName} is updated to ${newStockQty}`))
+                                    .catch(e => console.log(`Error: ${e}`))
+                                }
+                                else {
+                                    console.log(`'${answers.addInvQuantity}' is not a number`)
+                                }
+                            })
+                            break
+                        case "Add New Product":
+                            inq.prompt([
+                                {
+                                    name: 'product_name',
+                                    message: 'Enter Item Name'
+                                },
+                                {
+                                    name: 'department_name',
+                                    message: "Enter department"
+                                },
+                                {
+                                    name: 'price',
+                                    message: "Enter price"
+                                },
+                                {
+                                    name: 'stock_quantity',
+                                    message: "Enter current stock quantity"
+                                }
+                            ])
+                                .then(answers => {
                                     db.products.create({
                                         product_name: answers.product_name,
                                         department_name: answers.department_name,
                                         price: answers.price,
                                         stock_quantity: answers.stock_quantity
                                     })
-                            })
-                        
+                                        .then(() => console.log(`${answers.product_name} has been added.`))
+                                        .catch(e => console.log(`Error: ${e.original.sqlMessage}`))
+                                })
+
                             break
                         default:
                             process.exit()
-                        break
+                            break
                     }
+                    process.exit()
                 })
         })
+        .catch(e => console.log(e))
 })
